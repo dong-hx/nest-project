0188ad0d89deb9cfb3abf740b81c65b394d64757
donghx <799029218@qq.com>
2023-10-25 16:12:14 +0800

    feat: 修改数据库名称并连接到数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'nestjs_db',
+      port: 3306,
+      username: 'root',
+      password: 'a1234567',
+      database: 'nestjs_db',
+      entities: [User],
+      synchronize: true,
+    }),
+    CatsModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
